b8fd2542a52aa55f4a4d30e1902cba2033cf4c16
Alex - laptop <penoplavaaan@gmail.com>
2021-09-15 10:18:37 +0500

    Update 2.py

--- pisis/lab1/2.py
+++ pisis/lab1/2.py
@@ -2,7 +2,7 @@ _isOver = False
 namesArr = []
 
 while not _isOver:
-    newName = input('Введите нового человека в формате "Фамилия Имя Отчество Пол Стаж" или введите "0"')
+    newName = input('Введите нового человека в формате "Фамилия Имя Отчество Пол Стаж" или введите "0": ')
     if(newName) == '0':
         _isOver = not _isOver
     else:
@@ -11,8 +11,21 @@ while not _isOver:
         namesArr.append(newNameArr)
 
 namesArr = sorted(namesArr, key=lambda namesArr: namesArr[4])
-print('Самый "молодой":', namesArr[0], 'Самый "молодой":', namesArr[1])
+print('Самый "молодой":', namesArr[0], 'Самый "старый":', namesArr[len(namesArr)-1])
 
-sotr_male = []
-sotr_female = []
+sotr_male = [[0]]
+sotr_female = [[0]]
 
+start_letter = input('Введите волшебную букву: ')
+
+for sotr in namesArr:
+    if sotr[3] == 'М':
+        sotr_male.append(sotr)
+        if(sotr[1][0] == start_letter): sotr_male[0][0] +=1
+    else:
+        sotr_female.append(sotr)
+        if(sotr[1][0] == start_letter): sotr_female[0][0] +=1
+
+if(sotr_male[0][0] > sotr_female[0][0]): print('Мужиков больше')
+elif(sotr_male[0][0] > sotr_female[0][0]): print('Женщин больше')
+else: print('Поровну')
