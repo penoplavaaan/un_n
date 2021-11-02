4bf4ac783b0a1a7b7e78074c3b1fd78ecd872211
Alex - laptop <penoplavaaan@gmail.com>
2021-11-02 10:49:57 +0500

    add project rational

--- rtippo/Incapsulation.Weights/Indexer.cs
+++ rtippo/Incapsulation.Weights/Indexer.cs
@@ -8,83 +8,74 @@ namespace Incapsulation.Weights
 {
 	public class Indexer
     {
-        
-        public Indexer(double[] arr, int start, int length)
-        {
-            this.start = start;
-            this.length = length;
-            try
-            { 
-                if (this.length != 0)
-                {
-                    if (this.length > arr.Length)
-                    {
-                        throw new ArgumentException("range is invalid. Start = " + start + ", Length = " + length);
-                    }
-                    else
-                    {
-                        array = arr;
-                    }
-                    
-                }
-                
-                else array = new double[1];
-                 
-
-                if (length > arr.Length - this.start && this.length != 0 && this.start != 0) { 
-                    throw new ArgumentException("range is invalid. Start = " + start + ", Length = " + length);
-                }
-
-                Console.WriteLine();
-                int count = 0;
-                for (int i = this.start; i < this.start + this.length; i++)
-                {
-                    array[count] = arr[i];
-                    count++; 
-                }
-
-                ///
-                
-                Console.WriteLine("Valid range: " + this.start + " " + this.length);
-                foreach(double elem in array)
-                {
-                    Console.Write(elem + " ");
-                }
-                Console.WriteLine();
-                ///
-            }
-            catch(Exception e)
-            { 
-                throw new ArgumentException("range is invalid. Start = " + this.start + ", Length = " + this.length +"  Type is: " + e);
-            }  
-        }
-
-        private double[] array; 
-
+        private double[] array;
         private int start;
-        public int Start
-        {
-            get { return start; }
-            set { start = value; }
-        }
-
         private int length;
-        public int Get_Length
-        {
-            get { return length; }
-            set { length = value; }
-        }
-
-
-        public double this[int ind]
-        {
-            get { return array[ind]; }
-            set { array[ind] = value; }
-        }
-
         public int Length
         {
             get { return this.length; }
+        }
+
+        public Indexer(double[] arr, int start = 0, int length=0)
+        {
+            if (ValidateRange(arr, start, length))
+            {
+                array = arr;
+                this.start = start;
+                this.length = length;
+            }
+            else
+            {
+                throw new ArgumentException("Range is invalid.");
+            }    
+        }
+
+        
+
+        public double this[int ind]
+        {
+            get {
+                int lastIndex = GetLastIndex(ind);
+
+                if (LastIndexIsCorrect(lastIndex))
+                {
+                    return array[lastIndex];
+                }
+
+                else
+                {
+                    throw new IndexOutOfRangeException("Index is incorrect!");
+                }
+            }
+            set {
+                int lastIndex = GetLastIndex(ind);
+
+                if (LastIndexIsCorrect(lastIndex))
+                {
+                    array[lastIndex] = value;
+                }
+
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+
+
+        private bool ValidateRange(double[] arr, int start, int length)
+        {
+            return !(start < 0 || length < 0 || ((start + length) > arr.Length));
+        }
+
+        private bool LastIndexIsCorrect(int lastIndex)
+        {
+            return !(lastIndex > length || lastIndex < start);
+        }
+
+        private int GetLastIndex(int index)
+        {
+            return start + index;
         }
     }
 }
