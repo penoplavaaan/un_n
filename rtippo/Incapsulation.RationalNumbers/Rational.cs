f09007d82b40a7d1621ba8b7455fffd2e2698732
Alex - laptop <penoplavaaan@gmail.com>
2021-11-04 16:04:52 +0500

    rational done

--- rtippo/Incapsulation.RationalNumbers/Rational.cs
+++ rtippo/Incapsulation.RationalNumbers/Rational.cs
@@ -8,23 +8,25 @@ namespace Incapsulation.RationalNumbers
 {
     public class Rational
     {
-        public Rational(int Numerator, int Denominator)
+        public Rational(int numerator, int denominator = 1, bool isNan = false)
         {
-            (this.numerator, this.denominator) = (Numerator, Denominator);
-
-            if (Denominator % Numerator == 0 && Numerator != 1)
+            this.isNan = isNan;
+            if (denominator == 0)
             {
-                (this.numerator, this.denominator) = (Numerator / Numerator, Denominator / Numerator);
-
-                if(this.denominator < 0 && this.numerator > 0)
+                this.isNan = true;
+                (this.numerator, this.denominator) = (numerator, denominator);
+            } 
+            else
+            {
+                int gcd = Get_Greatest_Common_Divisor(numerator, denominator);
+                (this.numerator, this.denominator) = (numerator / gcd, denominator / gcd);
+                
+                if (this.denominator < 0 && this.numerator > 0)
                 {
                     this.numerator *= -1;
                     this.denominator *= -1;
-                }
+                } 
             }
-
-            this.isNan = this.Denominator == 0;
-         
         }
 
         private int numerator;
@@ -47,29 +49,75 @@ namespace Incapsulation.RationalNumbers
             get { return this.isNan; }
             set { this.isNan = value; }
         }
-
-        public Rational DivideOnInt(Rational a, int divider)
-        {
-            return new Rational(a.Numerator / divider, a.Denominator / divider);
-        }
+         
 
         public static Rational operator +(Rational a, Rational b)
+        { 
+            return new Rational(
+                a.Numerator * b.Denominator + b.Numerator*a.Denominator, 
+                a.Denominator * b.Denominator, 
+                (a.IsNan || b.IsNan)
+                );
+        }
+
+        public static Rational operator -(Rational a, Rational b)
         {
-            return new Rational(a.Numerator * b.Denominator + b.Numerator*a.Denominator, a.Denominator * b.Denominator);
+            return new Rational(
+                a.Numerator * b.Denominator - b.Numerator * a.Denominator, 
+                a.Denominator * b.Denominator, 
+                (a.IsNan || b.IsNan)
+                );
         }
 
         public static Rational operator *(Rational a, Rational b)
         {
-            return new Rational(a.Numerator * b.Numerator, a.Denominator * b.Denominator);
+            return new Rational(
+                a.Numerator * b.Numerator, 
+                a.Denominator * b.Denominator, 
+                (a.IsNan || b.IsNan)
+                );
         }
 
         public static Rational operator /(Rational a, Rational b)
         {
-            return new Rational(a.Numerator * b.Denominator, a.Denominator * b.Numerator); 
+            return new Rational(
+                a.Numerator * b.Denominator, 
+                a.Denominator * b.Numerator, 
+                (a.IsNan  || b.IsNan )
+                ); 
         }
 
-        
+        public static implicit operator double(Rational a)
+        {
+            return a.Denominator == 0 ? double.NaN : (double)(a.Numerator) / a.Denominator;
+        }
 
-        
+        public static explicit operator int(Rational a)
+        {
+            if (a.Numerator % a.Denominator == 0)
+            {
+                return a.Numerator / a.Denominator;
+            }
+            else
+            {
+                throw new System.Exception();
+            }
+        }
+
+        public static implicit operator Rational(int a)
+        {
+            return new Rational(a);
+        }
+
+        public int Get_Greatest_Common_Divisor(int a, int b)
+        {
+            if (a % b == 0)
+                return b;
+            if (b % a == 0)
+                return a;
+            if (a > b)
+                return Get_Greatest_Common_Divisor(a % b, b);
+            return Get_Greatest_Common_Divisor(a, b % a);
+        }
     }
 }
