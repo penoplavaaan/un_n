63eeedb52b3898985764ec20ccde7d01cc100561
Alex - laptop <penoplavaaan@gmail.com>
2021-11-06 08:55:02 +0500

    made all sound tests

--- rtippo/Inheritance/Inheritance.DataStructure/Category.cs
+++ rtippo/Inheritance/Inheritance.DataStructure/Category.cs
@@ -6,67 +6,72 @@ using System.Threading.Tasks;
 
 namespace Inheritance.DataStructure
 {
-    public class Category: IComparable
+    public class Category : IComparable
     {
         private string type;
         public string Type { get { return this.type; } set { this.type = value; } }
 
         private MessageType messageType;
-        public MessageType MessageType { get { return this.messageType; } set {this.messageType = value; } }
+        public MessageType MessageType { get { return this.messageType; } set { this.messageType = value; } }
 
         private MessageTopic messageTopic;
         public MessageTopic MessageTopic { get { return this.messageTopic; } set { this.messageTopic = value; } }
 
         public Category(string type, MessageType messageType, MessageTopic messageTopic)
         {
-            (this.type,this.messageType, this.messageTopic) = (type, messageType, messageTopic);
+            (this.type, this.messageType, this.messageTopic) = (type, messageType, messageTopic);
         }
 
         override public string ToString()
         {
-            return this.type.ToString() + '.'+ this.messageType.ToString() + '.' + this.messageTopic.ToString();
+            return this.type.ToString() + '.' + this.messageType.ToString() + '.' + this.messageTopic.ToString();
         }
 
         public int CompareTo(Category a)
         {
-            Console.WriteLine("Called on: " + this.ToString());
-            Console.WriteLine("With parameter: " + a.ToString());
+            if (this.type.CompareTo(a.type) != 0) return this.type.CompareTo(a.type);
 
-            if (this < a) return -1;
-            if (this == a) return 0;
-            return 1;
-            
-             
+            if (this.messageType.CompareTo(a.messageType) != 0) return this.messageType.CompareTo(a.messageType);
+
+            return this.messageTopic.CompareTo(a.messageTopic);
         }
 
-        public bool CompareStrings(string firts, string second)
+        public bool Equals(Category a)
         {
-            for (int i = 0; i < firts.Length; i++)
-            {
-                if (firts[i] > second[i]) return true;
-            }
-            return false;
+            return this == a;
         }
 
-         
 
 
-        public static bool operator >(Category a, Category b) {
-            return true;
+
+        public static bool operator >(Category a, Category b)
+        {
+            return Math.Sign(a.CompareTo(b)) == 1;
         }
 
-        public static bool operator <(Category a, Category b) {
-            return true;
+        public static bool operator <(Category a, Category b)
+        {
+            return Math.Sign(a.CompareTo(b)) == -1;
+        }
+
+        public static bool operator >=(Category a, Category b)
+        {
+            return a > b || a == b;
+        }
+
+        public static bool operator <=(Category a, Category b)
+        {
+            return a < b || a == b;
         }
 
         public static bool operator ==(Category a, Category b)
         {
-            return true;
+            return Math.Sign(a.CompareTo(b)) == 0;
         }
 
         public static bool operator !=(Category a, Category b)
         {
-            return true;
+            return Math.Sign(a.CompareTo(b)) != 0;
         }
 
     }
